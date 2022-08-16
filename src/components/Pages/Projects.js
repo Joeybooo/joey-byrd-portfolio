e0bdb2bb5ebc4ee8ab2ac00f0dbab9f626cca51e
Joey Byrd <joeybyrd9999@gmail.com>
2022-08-16 15:07:58 -0700

    added projectsinfo and projects page

---
+++ src/components/Pages/Projects.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Portfolio from "../Portfolio";
+import projects from "../../assets/projectsInfo.json";
+
+function ProjectsInfo() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ProjectsInfo;
