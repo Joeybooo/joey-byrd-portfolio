4fd74441e7e4e5cabdaee31e59607e955a6f1c55
Joey Byrd <joeybyrd9999@gmail.com>
2022-08-16 13:32:41 -0700

    added footer chance for further development

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import About from './components/About'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Header></Header>
         <About></About>
+        <Footer></Footer>
       </main>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Could add icons for links to socials like insta or github for later development
+function Footer() {
+    return (
+        <div className='footer bg-info'>
+            <p>Joey Byrd</p>
+        </div>
+    );
+}
+
+export default Footer;
