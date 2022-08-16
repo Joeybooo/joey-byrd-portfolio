6dcc87e9a253663df23feda704a7d9a1619e18f1
Joey Byrd <joeybyrd9999@gmail.com>
2022-08-16 13:25:16 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import './components/About'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from './components/About'
+import Header from './components/Header'
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Route exact path="/about" component={About}></Route>
-      </div>
-    </Router>
+    <div>
+      <main>
+        <Header></Header>
+        <About></About>
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className='header bg-info'>
+            <a href='https://github.com/Joeybooo'><h1 className='text-black'>Joey Byrd</h1></a> 
+        </header>
+    );
+}
+
+export default Header;
